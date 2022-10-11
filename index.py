a994d9088f7978f0087488480370dffec0682c19
MuhammadNurilHuda <64600830+MuhammadNurilHuda@users.noreply.github.com>
2022-10-11 20:01:05 +0700

    update 0.1.0

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World ! I'm still alive</p>"
+    return "<p>Hello, World! Namaku Nuril dan aku ganteng awikwok</p>"
