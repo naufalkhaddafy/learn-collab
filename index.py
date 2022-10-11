7fa24c1a2d6e0167d79678e90d8ba42216c0a718
naufalkhaddafy <naufalkhaddafy@webmail.umm.ac.id>
2022-10-11 20:16:01 +0700

    tes

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! Namaku Nuril dan aku ganteng asli bangget dan original</p>"
+    return "<p>Hello, World! Namaku Nuril dan aku ganteng asl bangget dan original</p>"
