82b1e8069ab219fbf0d540293498a06907480124
robbi621 <robbiadam@webmail.umm.ac.id>
2022-10-19 14:39:03 +0700

    heyoo

--- index.py
+++ index.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! Namaku Nuril dan gondrong aku ganteng asli dan original tapi boong. Gue Gondrong sayang </p>"
+    return "<p>Hello, World! Namaku Nuril dan gondrong aku ganteng asli dan original tapi boong. Gue Gondrong sayang Ditha lho</p>"
 
